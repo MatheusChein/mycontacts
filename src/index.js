78b91280e7769007e01d0a21cca2fdcf5902f815
Matheus Chein <matheus.chein@fabric.inc>
2022-02-10 13:32:25 -0300

    adding error handling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+require('express-async-errors');
+
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
-
 app.use(routes);
+app.use((error, request, response, _) => {
+  console.log('### Error Handler');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => {
   console.log('Server started at http://localhost:3000 🚀');
